f7db8df8640a98a10429c35517c937e64a535028
linshaolie <lshaolie@163.com>
2017-06-05 15:27:31 +0800

    Initial Commit

---
+++ DebuggingOverlayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DebuggingOverlayDemo
+//
+//  Created by shaolie on 2017/6/5.
+//  Copyright © 2017年 shaolie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
